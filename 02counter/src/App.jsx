cd3292be297bc8ac3a128862e3e631d6a5d92a85
prashantkumarpro <programmerkrprashant@gmail.com>
2023-08-23 14:45:08 +0530

    if count is 20 than stopped addvalue and if count is 0 than stopped removevalue

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const addValue = () => {
+    if (count === 20) return false;
+    setCount(count + 1)
+  }
+
+  const removeValue = () => {
+    if (count === 0) return false;
+    setCount(count - 1)
+  }
+  return (
+    <>
+
+
+      <div className="card">
+        <button onClick={addValue}>+
+        </button>
+        <p>count is {count}</p>
+      </div>
+      <div className="card">
+        <button onClick={removeValue}>-
+        </button>
+        <p>count is {count}</p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
