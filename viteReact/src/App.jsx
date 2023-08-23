cc0da83f3551ff647eb107225f76097715d8cb0a
prashantkumarpro <programmerkrprashant@gmail.com>
2023-08-23 13:06:23 +0530

    add h2 tag

--- viteReact/src/App.jsx
+++ viteReact/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <h1>Hello world !</h1>
+      <h2>Prashant Kumar</h2>
+
     </>
   )
 }
