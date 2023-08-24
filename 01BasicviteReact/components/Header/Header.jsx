4b0380a41e133153a55a13cb882626d2914c0788
prashantkumarpro <programmerkrprashant@gmail.com>
2023-08-24 20:20:04 +0530

    Next JS App
    react with next js app

---
+++ 01BasicviteReact/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="logo">
+            Logo
+        </div>
+        <div className="nav">
+            <h4>Home</h4>
+            <h4>About</h4>
+            <h4>Services</h4>
+            <h4>Blog</h4>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
--- 01BasicviteReact/src/main.jsx
+++ 01BasicviteReact/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import './App.css'
+import Header from '../components/Header/Header.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>,
 )
