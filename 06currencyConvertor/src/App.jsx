f056b528088872101d53de214645ce2b4bcaa032
Prashant Kumar <programmerkrprashant@gmail.com>
2023-09-24 21:25:23 +0530

    currencyInfo project  setup

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { InputBox } from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
